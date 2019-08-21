c64d49be05786863004f55ce3eff574dbe743ef7
Bernard Park <bernard.park1@gmail.com>
2019-08-21 11:27:10 -0400

    adding lombok and actuator

--- src/main/java/com/example/demo/entity/Customer.java
+++ src/main/java/com/example/demo/entity/Customer.java
@@ -5,7 +5,14 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class Customer {
 	
     @Id
@@ -14,38 +21,11 @@ public class Customer {
 	private String firstName;
 	private String lastName;
 	
-	public Customer() {
-	}
-
 	public Customer( String firstName, String lastName ) {
 		this.firstName = firstName;
 		this.lastName = lastName;
 	}
 	
-	public long getId() {
-		return id;
-	}
-
-	public void setId(long id) {
-		this.id = id;
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
 	@Override
 	public String toString() {
 		return String.format( "Customer[id=%d, firstName='%s', lastName='%s']", id, firstName, lastName );
